fe852aa8d3a62da051ecc1fc99200832c868191d
pedrosant <roberto19santana@gmail.com>
2020-02-01 23:50:49 -0300

    Atividade Concluida

--- questao4/Origem.cpp
+++ questao4/Origem.cpp
@@ -4,29 +4,25 @@ using namespace std;
 
 int main() {
 	int n;
-	int contador = 2;
+	int contador = 3;
 	int r = 1;
 	cout << "Digite um núemro: " << endl;
 	cin >> n;
-	n -= 3;
 	cout << "Resultado: ";
-	for (int i = 0; i <= n; i++) {
-		if (r == 1) {
-			cout << r << "|";
-			r += contador + 1;
-		}
-		if (r == 4) {
-			cout << r << "|";
-			contador += 3;
-			r += contador;
-			cout << r << "|";
-		}
-		else {
-			contador += 2;
-			r += contador;
-			cout << r << "|";
-		}
-		
+	if (n == 0) {
+		cout << "Digite um numero maior que zero";
 	}
+	else {
+		cout << r << "|";
+		r += 3;
+	}
+	if (n > 1) {
+		n -= 1;
+		for (int i = 0; i < n; i++) {		
+				cout << r << "|";
+				contador += 2;
+				r += contador;
+		}
+	}	
 	return 0;
 }
